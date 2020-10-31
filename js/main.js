3f4dd9d1673fb40b503ee3eb609ccf10b27fdf8c
gourav-143faz <gourav143faz@gmail.com>
2020-11-01 00:36:06 +0530

    goku

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 function day() {
 	document.querySelector("#mode_2").style.display="block";
-	document.querySelector("#mode_2").style.color="white";
-	document.querySelector("#ia1").style.color="white";
-	document.querySelector("#ia2").style.color="white";
-	document.querySelector("#ia3").style.color="white";
-	document.querySelector("#ia4").style.color="white";
-	document.querySelector("#ia5").style.color="white";
-	document.querySelector("#ib1").style.color="white";
-	document.querySelector("#ib2").style.color="white";
-	document.querySelector("#ib3").style.color="white";
-	document.querySelector("#ham").style.color="white";
 	document.querySelector("#mode_1").style.display="none";
 	document.querySelector("#header").style.backgroundColor="black";
+
+
+	var x, y;
+	x = document.querySelectorAll(".ia");
+	for(y = 0; y <x.length; y++){
+		x[y].style.color="white";
+	}
 }
 
 
@@ -19,27 +16,40 @@ function night() {
 	document.querySelector("#mode_1").style.display="block";
 	document.querySelector("#mode_2").style.display="none";
 	document.querySelector("#header").style.backgroundColor="#e8e8e8";
-	document.querySelector("#ia1").style.color="black";
-	document.querySelector("#ia2").style.color="black";
-	document.querySelector("#ia3").style.color="black";
-	document.querySelector("#ia4").style.color="black";
-	document.querySelector("#ia5").style.color="black";
-	document.querySelector("#ib1").style.color="black";
-	document.querySelector("#ib2").style.color="black";
-	document.querySelector("#ib3").style.color="black";
-	document.querySelector("#ham").style.color="black";
+
+
+	var x, y;
+	x = document.querySelectorAll(".ia");
+	for(y = 0; y <x.length; y++){
+		x[y].style.color="black";
+	}
 }
 
 
 
 
-function opn() {
+function ham_opn() {
 	document.querySelector("#ham_2").style.display="block";
 	document.querySelector("#ham_1").style.display="none";
 }
 
 
-function cls() {
+function ham_cls() {
 	document.querySelector("#ham_1").style.display="block";
 	document.querySelector("#ham_2").style.display="none";
+}
+
+
+function s_bar_opn() {
+	document.querySelector("#s_bar").style.display="block";
+	document.querySelector("#ib3b").style.display="block";
+	document.querySelector("#ib3b").style.color="white";
+	document.querySelector("#ib3").style.display="none";
+}
+
+
+function s_bar_cls() {
+	document.querySelector("#s_bar").style.display="none";
+	document.querySelector("#ib3b").style.display="none";
+	document.querySelector("#ib3").style.display="block";
 }
